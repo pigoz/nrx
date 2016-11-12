80d194c2a576d3903e569c259d2c0e5759091523
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2016-11-12 10:39:49 +0100

    add clang-format

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ all: build
 build: *.m
 	clang -Wall -o nrx nrx.m `pkg-config --libs --cflags mpv` -framework Cocoa -framework OpenGL
 
+fmt:
+	clang-format -i nrx.m
+
 clean:
 	rm nrx
--- nrx.m
+++ nrx.m
@@ -1,12 +1,11 @@
 #import <mpv/client.h>
 #import <mpv/opengl_cb.h>
 
+#import <OpenGL/gl.h>
 #import <stdio.h>
 #import <stdlib.h>
-#import <OpenGL/gl.h>
 
 #import <Cocoa/Cocoa.h>
-
 
 static inline void check_error(int status)
 {
@@ -16,18 +15,18 @@ static inline void check_error(int status)
     }
 }
 
-static void *get_proc_address(void *ctx, const char *name)
+static void* get_proc_address(void* ctx, const char* name)
 {
     CFStringRef symbolName = CFStringCreateWithCString(kCFAllocatorDefault, name, kCFStringEncodingASCII);
-    void *addr = CFBundleGetFunctionPointerForName(CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengl")), symbolName);
+    void* addr = CFBundleGetFunctionPointerForName(CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengl")), symbolName);
     CFRelease(symbolName);
     return addr;
 }
 
-static void glupdate(void *ctx);
+static void glupdate(void* ctx);
 
 @interface MpvClientOGLView : NSOpenGLView
-@property mpv_opengl_cb_context *mpvGL;
+@property mpv_opengl_cb_context* mpvGL;
 - (instancetype)initWithFrame:(NSRect)frame;
 - (void)drawRect;
 - (void)fillBlack;
@@ -46,7 +45,7 @@ static void glupdate(void *ctx);
                     pixelFormat:[[NSOpenGLPixelFormat alloc] initWithAttributes:attributes]];
 
     if (self) {
-        [self setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
+        [self setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
         // swap on vsyncs
         GLint swapInt = 1;
         [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
@@ -78,14 +77,15 @@ static void glupdate(void *ctx);
 @end
 
 @interface CocoaWindow : NSWindow
-@property(retain, readonly) MpvClientOGLView *glView;
-@property(retain, readonly) NSButton *pauseButton;
+@property (retain, readonly) MpvClientOGLView* glView;
+@property (retain, readonly) NSButton* pauseButton;
 @end
 
 @implementation CocoaWindow
 - (BOOL)canBecomeMainWindow { return YES; }
 - (BOOL)canBecomeKeyWindow { return YES; }
-- (void)initOGLView {
+- (void)initOGLView
+{
     NSRect bounds = [[self contentView] bounds];
     // window coordinate origin is bottom left
     NSRect glFrame = NSMakeRect(bounds.origin.x, bounds.origin.y + 30, bounds.size.width, bounds.size.height - 30);
@@ -104,21 +104,20 @@ static void glupdate(void *ctx);
 }
 @end
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
-{
-    mpv_handle *mpv;
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    mpv_handle* mpv;
     dispatch_queue_t queue;
-    CocoaWindow *window;
+    CocoaWindow* window;
 }
 @end
 
-static void wakeup(void *);
+static void wakeup(void*);
 
 @implementation AppDelegate
 
-- (void)createWindow {
-    int mask = NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|
-        NSWindowStyleMaskMiniaturizable|NSWindowStyleMaskResizable;
+- (void)createWindow
+{
+    int mask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
 
     window = [[CocoaWindow alloc]
         initWithContentRect:NSMakeRect(0, 0, 1280, 720)
@@ -133,130 +132,127 @@ static void wakeup(void *);
     [window makeMainWindow];
     [window makeKeyAndOrderFront:nil];
 
-    NSMenu *m = [[NSMenu alloc] initWithTitle:@"AMainMenu"];
-    NSMenuItem *item = [m addItemWithTitle:@"Apple" action:nil keyEquivalent:@""];
-    NSMenu *sm = [[NSMenu alloc] initWithTitle:@"Apple"];
+    NSMenu* m = [[NSMenu alloc] initWithTitle:@"AMainMenu"];
+    NSMenuItem* item = [m addItemWithTitle:@"Apple" action:nil keyEquivalent:@""];
+    NSMenu* sm = [[NSMenu alloc] initWithTitle:@"Apple"];
     [m setSubmenu:sm forItem:item];
-    [sm addItemWithTitle: @"quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    [sm addItemWithTitle:@"quit" action:@selector(terminate:) keyEquivalent:@"q"];
     [NSApp setMenu:m];
     [NSApp activateIgnoringOtherApps:YES];
 }
 
-- (void) applicationDidFinishLaunching:(NSNotification *)notification {
-            [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-            atexit_b(^{
-                    // Because activation policy has just been set to behave like a real
-                    // application, that policy must be reset on exit to prevent, among
-                    // other things, the menubar created here from remaining on screen.
-                    [NSApp setActivationPolicy:NSApplicationActivationPolicyProhibited];
-                    });
+- (void)applicationDidFinishLaunching:(NSNotification*)notification
+{
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    atexit_b(^{
+        // Because activation policy has just been set to behave like a real
+        // application, that policy must be reset on exit to prevent, among
+        // other things, the menubar created here from remaining on screen.
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyProhibited];
+    });
 
-            // Read filename
-            NSArray *args = [NSProcessInfo processInfo].arguments;
-            if (args.count < 2) {
-                NSLog(@"Expected filename on command line");
-                exit(1);
-            }
-            NSString *filename = args[1];
+    // Read filename
+    NSArray* args = [NSProcessInfo processInfo].arguments;
+    if (args.count < 2) {
+        NSLog(@"Expected filename on command line");
+        exit(1);
+    }
+    NSString* filename = args[1];
 
-            [self createWindow];
-            window.pauseButton.target = self;
+    [self createWindow];
+    window.pauseButton.target = self;
 
-            mpv = mpv_create();
-            if (!mpv) {
-                printf("failed creating context\n");
-                exit(1);
-            }
+    mpv = mpv_create();
+    if (!mpv) {
+        printf("failed creating context\n");
+        exit(1);
+    }
 
-            check_error(mpv_set_option_string(mpv, "input-media-keys", "yes"));
-            // request important errors
-            check_error(mpv_request_log_messages(mpv, "warn"));
+    check_error(mpv_set_option_string(mpv, "input-media-keys", "yes"));
+    // request important errors
+    check_error(mpv_request_log_messages(mpv, "warn"));
 
-            check_error(mpv_initialize(mpv));
-            check_error(mpv_set_option_string(mpv, "vo", "opengl-cb"));
-            mpv_opengl_cb_context *mpvGL = mpv_get_sub_api(mpv, MPV_SUB_API_OPENGL_CB);
-            if (!mpvGL) {
-                puts("libmpv does not have the opengl-cb sub-API.");
-                exit(1);
-            }
-            // pass the mpvGL context to our view
-            window.glView.mpvGL = mpvGL;
-            int r = mpv_opengl_cb_init_gl(mpvGL, NULL, get_proc_address, NULL);
-            if (r < 0) {
-                puts("gl init has failed.");
-                exit(1);
-            }
-            mpv_opengl_cb_set_update_callback(mpvGL, glupdate, (__bridge void *)window.glView);
+    check_error(mpv_initialize(mpv));
+    check_error(mpv_set_option_string(mpv, "vo", "opengl-cb"));
+    mpv_opengl_cb_context* mpvGL = mpv_get_sub_api(mpv, MPV_SUB_API_OPENGL_CB);
+    if (!mpvGL) {
+        puts("libmpv does not have the opengl-cb sub-API.");
+        exit(1);
+    }
+    // pass the mpvGL context to our view
+    window.glView.mpvGL = mpvGL;
+    int r = mpv_opengl_cb_init_gl(mpvGL, NULL, get_proc_address, NULL);
+    if (r < 0) {
+        puts("gl init has failed.");
+        exit(1);
+    }
+    mpv_opengl_cb_set_update_callback(mpvGL, glupdate, (__bridge void*)window.glView);
 
-            // Deal with MPV in the background.
-            queue = dispatch_queue_create("mpv", DISPATCH_QUEUE_SERIAL);
-            dispatch_async(queue, ^{
-                    // Register to be woken up whenever mpv generates new events.
-                    mpv_set_wakeup_callback(mpv, wakeup, (__bridge void *)self);
-                    // Load the indicated file
-                    const char *cmd[] = {"loadfile", filename.UTF8String, NULL};
-                    check_error(mpv_command(mpv, cmd));
-                    });
+    // Deal with MPV in the background.
+    queue = dispatch_queue_create("mpv", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{
+        // Register to be woken up whenever mpv generates new events.
+        mpv_set_wakeup_callback(mpv, wakeup, (__bridge void*)self);
+        // Load the indicated file
+        const char* cmd[] = { "loadfile", filename.UTF8String, NULL };
+        check_error(mpv_command(mpv, cmd));
+    });
 }
 
-static void glupdate(void *ctx)
+static void glupdate(void* ctx)
 {
-    MpvClientOGLView *glView = (__bridge MpvClientOGLView *)ctx;
+    MpvClientOGLView* glView = (__bridge MpvClientOGLView*)ctx;
     // I'm still not sure what the best way to handle this is, but this
     // works.
     dispatch_async(dispatch_get_main_queue(), ^{
-            [glView drawRect];
-            });
+        [glView drawRect];
+    });
 }
 
-- (void) handleEvent:(mpv_event *)event
+- (void)handleEvent:(mpv_event*)event
 {
     switch (event->event_id) {
-        case MPV_EVENT_SHUTDOWN: {
-                                     mpv_detach_destroy(mpv);
-                                     mpv_opengl_cb_uninit_gl(window.glView.mpvGL);
-                                     mpv = NULL;
-                                     printf("event: shutdown\n");
-                                     break;
-                                 }
+    case MPV_EVENT_SHUTDOWN: {
+        mpv_detach_destroy(mpv);
+        mpv_opengl_cb_uninit_gl(window.glView.mpvGL);
+        mpv = NULL;
+        printf("event: shutdown\n");
+        break;
+    }
 
-        case MPV_EVENT_LOG_MESSAGE: {
-                                        struct mpv_event_log_message *msg = (struct mpv_event_log_message *)event->data;
-                                        printf("[%s] %s: %s", msg->prefix, msg->level, msg->text);
-                                    }
+    case MPV_EVENT_LOG_MESSAGE: {
+        struct mpv_event_log_message* msg = (struct mpv_event_log_message*)event->data;
+        printf("[%s] %s: %s", msg->prefix, msg->level, msg->text);
+    }
 
-        default:
-  printf("event: %s\n", mpv_event_name(event->event_id));
+    default:
+        printf("event: %s\n", mpv_event_name(event->event_id));
     }
 }
 
-- (void)togglePause:(NSButton *)button
+- (void)togglePause:(NSButton*)button
 {
     if (mpv) {
         switch (button.state) {
-            case NSOffState:
-                {
-                    int pause = 0;
-                    mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, &pause);
-                }
-                break;
-            case NSOnState:
-                {
-                    int pause = 1;
-                    mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, &pause);
-                }
-                break;
-            default:
-                NSLog(@"This should never happen.");
+        case NSOffState: {
+            int pause = 0;
+            mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, &pause);
+        } break;
+        case NSOnState: {
+            int pause = 1;
+            mpv_set_property(mpv, "pause", MPV_FORMAT_FLAG, &pause);
+        } break;
+        default:
+            NSLog(@"This should never happen.");
         }
     }
 }
 
-- (void) readEvents
+- (void)readEvents
 {
     dispatch_async(queue, ^{
         while (mpv) {
-            mpv_event *event = mpv_wait_event(mpv, 0);
+            mpv_event* event = mpv_wait_event(mpv, 0);
             if (event->event_id == MPV_EVENT_NONE)
                 break;
             [self handleEvent:event];
@@ -264,22 +260,22 @@ static void glupdate(void *ctx)
     });
 }
 
-static void wakeup(void *context)
+static void wakeup(void* context)
 {
-    AppDelegate *a = (__bridge AppDelegate *) context;
+    AppDelegate* a = (__bridge AppDelegate*)context;
     [a readEvents];
 }
 
 // quit when the window is closed.
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)app
 {
     return YES;
 }
 
-- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
+- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication*)sender
 {
     NSLog(@"Terminating.");
-    const char *args[] = {"quit", NULL};
+    const char* args[] = { "quit", NULL };
     mpv_command(mpv, args);
     [window.glView clearGLContext];
     return NSTerminateNow;
@@ -288,10 +284,11 @@ static void wakeup(void *context)
 @end
 
 // Delete this if you already have a main.m.
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[])
+{
     @autoreleasepool {
-        NSApplication *app = [NSApplication sharedApplication];
-        AppDelegate *delegate = [AppDelegate new];
+        NSApplication* app = [NSApplication sharedApplication];
+        AppDelegate* delegate = [AppDelegate new];
         app.delegate = delegate;
         [app run];
     }
